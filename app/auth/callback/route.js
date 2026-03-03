cf1c75c40ed9ce9c4408cc5e3428e014118a0738
ramdane1983-droid <ramdane1983@gmail.com>
2026-03-03 17:15:45 +0100

    remove auth callback

--- app/auth/callback/route.js
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export async function GET(request) {
-  const requestUrl = new URL(request.url)
-  return NextResponse.redirect(new URL('/dashboard', requestUrl.origin))
-}
