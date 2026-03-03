42849faad7a37b2e9672e6fe0ef6b25c067bd2c2
ramdane1983-droid <ramdane1983@gmail.com>
2026-03-03 13:05:22 +0100

    disable middleware test

--- middleware.js
+++ middleware.js
@@ -1,12 +1,6 @@
 import { NextResponse } from 'next/server'
 
 export async function middleware(req) {
-  const token = req.cookies.get('sb-access-token')
-  
-  if (!token && req.nextUrl.pathname.startsWith('/dashboard')) {
-    return NextResponse.redirect(new URL('/login', req.url))
-  }
-  
   return NextResponse.next()
 }
 
